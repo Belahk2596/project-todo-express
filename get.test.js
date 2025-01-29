e5890cfd1efd6f38f4981757467b29db8df5fd83
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2025-01-29 16:46:38 +1300

    imported test and expect created some mock tests to run with bun test

--- get.test.js
+++ get.test.js
@@ -1,10 +1,21 @@
 import { test, expect } from "bun:test";
 
 function sum (a, b) {
-    return a + b;
-}
+    return a / b;
+};
 
 test("add 1 + 2", () => {
     expect(sum(1,2)).toBe(3)
 });
 
+test("multiply 1 x 2", () => {
+    expect(sum(1,2)).toBe(2)
+});
+
+test("divide 2 / 2", () => {
+    expect(sum(2,2)).toBe(1)
+});
+
+test("subtract 2 - 1", () => {
+    expect(sum(2,1)).toBe(1)
+});
